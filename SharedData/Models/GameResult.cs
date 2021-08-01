8bc02bf2ad6c8d011bbc961b8f5a3d0645dce915
hakrimryu <hakrimryu@gmail.com>
2021-08-01 12:33:37 +0900

    Game ResultをShared Dataに、コピーとプロジェクトを参照

---
+++ SharedData/Models/GameResult.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharedData.Models
+{
+    class GameResult
+    {
+        /// <summary>
+        /// DB ID
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// User ID
+        /// </summary>
+        public int UserId { get; set; }
+        /// <summary>
+        /// User名
+        /// </summary>
+        public string UserName { get; set; }
+        /// <summary>
+        /// スコア
+        /// </summary>
+        public int Score { get; set; }
+        /// <summary>
+        /// 時間
+        /// </summary>
+        public DateTime Data { get; set; }
+    }
+}
