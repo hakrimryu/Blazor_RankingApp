7ad41d79079944c8a267ed1244fa12628e875a41
hakrimryu <hakrimryu@gmail.com>
2021-08-05 22:09:33 +0900

    RankingControllerのRead機能作成

---
+++ WebApi/Controllers/RankingController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.Data;
+using SharedData.Models;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RankingController : ControllerBase
+    {
+        ApplicationDbContext _context;
+
+        public RankingController(ApplicationDbContext context)
+        {
+            this._context = context;
+        }
+
+        // Create
+
+        // Read
+        [HttpGet]
+        public List<GameResult> GetGameResults()
+        {
+            List<GameResult> results = _context.GameResults
+                .OrderByDescending(item => item.Score)
+                .ToList();
+
+            return results;
+        }
+
+        [HttpGet("{id}")]
+        public GameResult GetGameResult(int id)
+        {
+            GameResult result = _context.GameResults
+                .Where(item => item.Id == id)
+                .FirstOrDefault();
+
+            return result;
+        }
+
+
+
+        // Update
+
+        // Delete
+    }
+}
